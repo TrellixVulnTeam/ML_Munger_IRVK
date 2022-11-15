ffcb8a8839edc05a6eda596d3604c69f99e9dcd3
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-15 20:55:44 +0000

    Adding tarfile member sanitization to extractall()

--- DataMunger/Compression/archive.py
+++ DataMunger/Compression/archive.py
@@ -66,7 +66,29 @@ def to_7z(source, output):
 # Decompress the archive.
 def un_tar(source, output):
     with tarfile.open(source) as tar:
-        tar.extractall(output)
+        
+        import os
+        
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tar, output)
     
 def un_zip(source, output):
     paths = fo.create_dir_tree_list(source)
